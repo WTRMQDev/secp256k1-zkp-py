9cdd71e90e129b907a64b2f4a10d8590447f8c9b
Quirinus Quirrell <Melanchthon@tutanota.com>
2017-09-09 10:30:57 +0000

    Remove Schnorr modules

--- _cffi_build/build.py
+++ _cffi_build/build.py
@@ -37,7 +37,7 @@ _base = [Source(absolute("_cffi_build/secp256k1.h"), "#include <secp256k1.h>", )
 _modules = {
     'ecdh': Source(absolute("_cffi_build/secp256k1_ecdh.h"), "#include <secp256k1_ecdh.h>", ),
     'recovery': Source(absolute("_cffi_build/secp256k1_recovery.h"), "#include <secp256k1_recovery.h>", ),
-    'schnorr': Source(absolute("_cffi_build/secp256k1_schnorr.h"), "#include <secp256k1_schnorr.h>", ),
+#    'schnorr': Source(absolute("_cffi_build/secp256k1_schnorr.h"), "#include <secp256k1_schnorr.h>", ),
     'rangeproof': Source(absolute("_cffi_build/secp256k1_rangeproof.h"), "#include <secp256k1_rangeproof.h>", ),
 }
 
